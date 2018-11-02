b3f38eca9faf8dac8a242b7eb20e2bd9253de253
dariasteam <dariasteam94@gmail.com>
2018-11-02 10:14:05 +0000

    Add scripts

---
+++ scripts/light_slider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class light_slider : MonoBehaviour {	
+
+	public Light light;
+	private Slider sl;
+	// Use this for initialization
+	void Start () {		
+	        sl = GetComponent<Slider>();
+		sl.onValueChanged.AddListener(delegate {ValueChangeCheck(); });		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ValueChangeCheck() {
+		light.intensity = sl.value;
+	}
+
+
+	
+}
---
+++ scripts/sphereSlider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class sphereSlider : MonoBehaviour {	
+
+	public GameObject sphere;
+	private Slider sl;
+	// Use this for initialization
+	void Start () {		
+	        sl = GetComponent<Slider>();
+		sl.onValueChanged.AddListener(delegate {ValueChangeCheck(); });		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ValueChangeCheck() {			
+		sphere.transform.localScale = Vector3.one * sl.value;
+		print (sphere.transform.localScale);
+	}
+
+
+	
+}
